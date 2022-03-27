73692375ffc3b703cc0ec15a3aff138918df6459
Nebiyu Samuel <neba.samuel17@gmail.com>
2022-03-27 12:06:58 +0300

    adds function for calclating to the right and move

--- src/position.py
+++ src/position.py
@@ -1,12 +1,25 @@
 class Position:
      def __init__(self, position: position, initialPosition: initialposition):
         self.position: position = position
-        self.currentPosition: initialPosition = initialPosition
+        self.currentPosition: Position = initialPosition
 
      def Left(self) -> Position:
         newOrientation = self.currentPosition.orientation
-        newPosition = initialPosition(self.currentPosition.coordinateInX,
+        newPosition = Position(self.currentPosition.coordinateInX,
                                     self.currentPosition.coordinateInY,
                                     newOrientation)
         self.currentPosition = newPosition
         return newPosition
+     def Right(self) -> Position:
+        newOrientation = self.currentPosition.orientation
+        newPosition = Position(self.currentPosition.coordinateInX,
+                                    self.currentPosition.coordinateInY,
+                                    newOrientation)
+        self.currentPosition = newPosition
+        return newPosition
+
+     def move(self) -> Position:
+        
+        newRoverPosition = self.currentPosition.orientation
+        self.currentPosition = newRoverPosition
+        return self.currentPosition
