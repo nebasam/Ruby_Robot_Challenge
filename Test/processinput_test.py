5493634137228478a22a6389cd17d74a4b0e4e7b
Nebiyu Samuel <neba.samuel17@gmail.com>
2022-03-30 01:48:28 +0300

    computes test script for executeinstruction script

--- Test/processinput_test.py
+++ Test/processinput_test.py
@@ -6,16 +6,17 @@ sys.path.append(os.path.abspath(os.path.join('../src')))
 
 from processinput import ProcessInput
 
+class Test_executeinstruction:
 
-def test_shouldCalculateFinalPositionForTwoRubyRobots(capsys):
-    # Given
-    inputFile = '../inputFile.txt'
-    processinput = ProcessInput(inputFile)
+    def test_shouldCalculateFinalPositionForTwoRubyRobots(capsys):
+        # Given
+        inputFile = '../inputFile.txt'
+        processinput = ProcessInput(inputFile)
     
     
-    # When
-    processinput.processInstructions()
-    printedOutput = capsys.readouterr().out
-    # Then
-    expectedRoversFinalPositions = '1 2 W' + '\n' + '3 2 E' + '\n'
-    assert expectedRoversFinalPositions == printedOutput
+        # When
+        processinput.processInstructions()
+        printedOutput = capsys.readouterr().out
+        # Then
+        expectedRubyFinalPositions = '1 2 W' + '\n' + '3 2 E' + '\n'
+        assert expectedRubyFinalPositions == printedOutput
